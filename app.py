cb4c76a27e8b68bbb187094ea76694098a2e54c9
leohernandez01 <leojavihp@hotmail.com>
2023-03-16 22:59:36 -0600

    Agregadas las capturas de pantalla del juego a la pagina web

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def home():
   return render_template('index.html')
 
+@app.route("/game")
+def game():
+  return render_template('game.html')
+
 if __name__ == "__main__":
   app.run(debug=True)
 
